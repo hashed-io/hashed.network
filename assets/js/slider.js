3f3efec9f4852a51b12d68ced30b18d13e16fc97
alexisCode50 <skrillex.ja14@gmail.com>
2022-07-07 14:25:18 -0500

    changes in banner, slider team and footer

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,7 @@
 // Drag scroll desktop
 const sb = new ScrollBooster({
-    viewport: document.querySelector('.team'),
-    content: document.querySelector('.team .members'),
+    viewport: document.querySelector('.team .scroll-members'),
+    content: document.querySelector('.team .scroll-members .members'),
     scrollMode: 'transform',
     direction: 'horizontal'
 })
@@ -11,21 +11,8 @@ const arrows = document.querySelectorAll('.arrows li')
 const team = document.querySelectorAll('.members .item')
 var widthTeamContainer = 0
 team.forEach(item => {
-    widthTeamContainer += item.offsetWidth
+    widthTeamContainer += (item.offsetWidth - 150)
 })
-
-const desktop = window.matchMedia('(max-width: 1500px)')
-const phone = window.matchMedia('(max-width: 500px)')
-
-if (desktop.matches) {
-    widthTeamContainer -= 800
-}
-
-if (phone.matches) {
-    widthTeamContainer += 800
-}
-
-console.log(widthTeamContainer)
 
 const moveToLeft = (ctx) => {
     const label = ctx.getAttribute('data-label')
